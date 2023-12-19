2b187c65547217428d9517fec4c7578ad3fd5244
conaryHernandez <conaryh@gmail.com>
2023-12-19 11:49:40 -0600

    feat: add meals component

--- food-community/app/meals/page.jsx
+++ food-community/app/meals/page.jsx
@@ -1,3 +1,26 @@
+import Link from 'next/link';
+
+import classes from './page.module.css';
+
 export default function Meals() {
-  return <h1>Meals</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals
+          <span className={classes.highlight}>by you!</span>
+        </h1>
+
+        <p>
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home.
+        </p>
+
+        <p className={classes.cta}>
+          <Link href='meals/share'>Share your own meal!</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }
