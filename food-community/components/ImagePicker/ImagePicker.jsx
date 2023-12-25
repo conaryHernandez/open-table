846533cec566a84ee5f8f01e69813458e7550921
conaryHernandez <conaryh@gmail.com>
2023-12-25 12:08:46 -0600

    feat: add save meal server action

--- food-community/components/ImagePicker/ImagePicker.jsx
+++ food-community/components/ImagePicker/ImagePicker.jsx
@@ -1,13 +1,32 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import Image from 'next/image';
 import classes from './ImagePicker.module.css';
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const inputRef = useRef();
 
   function handlePickClick() {
     inputRef.current.click();
+  }
+
+  function handlePickImage(e) {
+    const pickedFile = e.target.files[0];
+
+    if (!pickedFile) {
+      setPickedImage(null);
+      return;
+    }
+
+    const file = new FileReader();
+
+    file.onload = () => {
+      setPickedImage(file.result);
+    };
+
+    file.readAsDataURL(pickedFile);
   }
 
   return (
@@ -15,13 +34,20 @@ export default function ImagePicker({ label, name }) {
       <label htmlFor={name}>{label}</label>
 
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {pickedImage && <Image src={pickedImage} alt='Preview' fill />}
+          {!pickedImage && <p>Please pick an image.</p>}
+        </div>
+
         <input
-          ref={inputRef}
-          className={classes.input}
-          type='file'
           id={name}
+          ref={inputRef}
+          type='file'
           name={name}
+          required
           accept='image/png, image/jpeg'
+          className={classes.input}
+          onChange={handlePickImage}
         />
         <button
           className={classes.button}
--- food-community/lib/meals.js
+++ food-community/lib/meals.js
@@ -1,4 +1,7 @@
+import fs from 'fs';
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
@@ -10,4 +13,54 @@ export async function getAllMeals() {
 
 export async function getMealBySlug(slug) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export async function saveMeal(meal) {
+  const slug = slugify(meal.title, { lower: true });
+
+  const extention = meal.image?.name?.split('.').pop();
+  const filename = `${slug}.${extention}`;
+  const filepath = `public/images/${filename}`;
+
+  const stream = fs.createWriteStream(filepath);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error('Saving image failed');
+    }
+  });
+
+  const result = db
+    .prepare(
+      `
+    INSERT INTO meals (
+      title,
+      slug,
+      summary,
+      instructions,
+      image,
+      creator,
+      creator_email
+    ) VALUES (
+      @title,
+      @slug,
+      @summary,
+      @instructions,
+      @image,
+      @creator,
+      @creator_email
+    )
+  `
+    )
+    .run({
+      ...meal,
+      slug,
+      image: `/images/${filename}`,
+      title: xss(meal.title),
+      summary: xss(meal.summary),
+      instructions: xss(meal.instructions),
+    });
+
+  return result.lastInsertRowid;
 }
